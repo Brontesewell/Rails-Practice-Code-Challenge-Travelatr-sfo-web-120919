a5dbc06351955515ce68e4cdff3dd75951e301f2
github username <github email address>
2020-01-15 17:18:52 -0800

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,19 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+    validates :name, uniqueness: true, presence: true
+    validates :age, numericality: { greater_than: 0 }
+    validates :bio, length: { minimum: 30 }
+
+
+    def featured_post
+        self.posts.order(likes: :desc)[0]
+      end
+    
+      def total_likes
+        self.posts.inject(self.posts[0].likes) do |sum, post|
+            sum + post.likes
+        end
+      end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
-
+    belongs_to :blogger
+    belongs_to :destination
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :content, length: { maximum: 100 }
+    
 end
